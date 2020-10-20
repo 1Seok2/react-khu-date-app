193e2ac9fcb4e30c42f85d65e5bbb53367859f66
1seok2 <misohaha31@naver.com>
2020-10-20 10:37:18 +0900

    add ex of styled components

---
+++ src/components/page/home/Home.styled.ts
@@ -0,0 +1,7 @@
+/* eslint-disable @typescript-eslint/explicit-function-return-type */
+import styled from 'styled-components';
+
+export const Hello = styled.button`
+  background-color: #a37c43;
+  color: white;
+`;
