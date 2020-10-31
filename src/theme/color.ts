2d3d439400d8fc3473f36c725b231a50cfa8a5f4
1seok2 <misohaha31@naver.com>
2020-10-31 16:00:34 +0900

    add theme color

--- src/theme/color.ts
+++ src/theme/color.ts
@@ -1,6 +1,6 @@
 export const color = {
   black: '#333', // 공용 검은색
-  date: '#f6e58d', // 웹 테마 색
+  date: '#CA7B67', // 웹 테마 색
   datelight: '#a9fece', // 웹 테마 밝은 색
   datedark: '#4d74a9', // 웹 테마 어두운 색
   lightgray: '#cfcfcf',
